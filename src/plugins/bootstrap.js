9a16b08ac6518c9111c6ab73070dd0dac31e01af
kyun <yezi0u0@gmail.com>
2019-10-18 16:08:38 +0800

    modify home page style

--- src/plugins/bootstrap.js
+++ src/plugins/bootstrap.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGithub, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithub, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faGithubSquare, faGithub)
+library.add(faGithubSquare, faGithub, faCalendarAlt)
 
 Vue.use(BootstrapVue)
 Vue.component('fa-icon', FontAwesomeIcon)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,24 +3,26 @@
     <b-col :lg="12" :xl="8" class="divider">
       <article>
         <header>
-          <h1>article元素使用方法</h1>
-          <p>发表日期：
+          <h3>article元素使用方法</h3>
+          <p>
+            <fa-icon :icon="['fas', 'calendar-alt']"></fa-icon>
+             发表日期：
             <time pubdate="pubdate">2017/2/9</time>
           </p>
         </header>
         <p>此标签里显示的是article整个文章的主要内容，，下面的section元素里是对该文章的评论</p>
         <section>
-          <h2>评论</h2>
+          <h3>评论</h3>
           <article>
             <header>
-              <h3>发表者：Galin</h3>
+              <h5>发表者：Galin</h5>
               <p>1小时前</p>
             </header>
             <p>这篇文章很不错啊，顶一下！</p>
           </article>
           <article>
             <header>
-              <h3>发表者：木木</h3>
+              <h5>发表者：木木</h5>
               <p>1小时前</p>
             </header>
             <p>这篇文章很不错啊，对article解释的很详细</p>
