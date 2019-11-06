8767f65498b5258d957257b6d13d2711710b1646
kyun <yezi0u0@gmail.com>
2019-11-06 10:02:56 +0800

    update index

--- pages/articles/page/_number.vue
+++ pages/articles/page/_number.vue
@@ -1,5 +1,38 @@
 <template>
-  <div></div>
+  <div>
+    <b-row>
+      <b-col :lg="12" :xl="8" class="divider">
+        <article v-for="(article, index) in articles" :key="index">
+          <header>
+            <h4>{{ article.title }}</h4>
+            <p>
+              <fa-icon :icon="['fas', 'calendar-alt']" />&nbsp;
+              <time>{{ new Date(article.postDate).toLocaleString() }}</time>
+            </p>
+          </header>
+          <p>
+            {{ article.content }}
+          </p>
+          <section>
+            <br />
+            <h4><fa-icon :icon="['fas', 'comment-dots']" />&nbsp; Comments</h4>
+            <article v-for="(comment, idx) in article.comments" :key="idx">
+              <header>
+                <h5>author：{{ comment.user.name }}</h5>
+                <p>{{ new Date(comment.createDate).toLocaleString() }}</p>
+              </header>
+              <p>{{ comment.content }}</p>
+            </article>
+          </section>
+        </article>
+      </b-col>
+      <b-col :lg="12" :xl="4">
+        <div>
+          <span>侧栏</span>
+        </div>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,16 @@
 <template>
   <div>
-    <b-carousel controls indicators img-width="1920" img-height="500">
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1920/500/?image=52"
-      ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1920/500/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide
-        img-src="https://picsum.photos/1920/500/?image=58"
-      ></b-carousel-slide>
-    </b-carousel>
+    <first-page />
   </div>
 </template>
 
 <script>
+import firstPage from '@/pages/articles/page/_number'
+
 export default {
-  layout: 'blog',
+  components: {
+    firstPage
+  },
   data() {
     return {}
   }
