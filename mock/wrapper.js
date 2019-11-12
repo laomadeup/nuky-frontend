b89a76423df305a15524229d40809cc0b2f231ad
kyun <yezi0u0@gmail.com>
2019-11-12 16:31:46 +0800

    mock paged query

--- mock/wrapper.js
+++ mock/wrapper.js
@@ -17,13 +17,14 @@ function pageRes(req, res) {
   const queryString = url.substring(url.indexOf('?') + 1)
   const queries = require('querystring').parse(queryString)
   const curPage = Number(queries[PAGE_FLAG])
+  const pageable = { pageNumber: curPage - 1 }
   const totalCount = res.get('X-Total-Count')
   const data = res.locals.data
   const totalPages = Math.ceil(totalCount / PAGE_LIMIT)
   res.jsonp({
     content: data,
     totalPages,
-    curPage
+    pageable
   })
 }
 
