6f82769f4b57ab76c811b83f9ea1eda107c281f1
kyun <yezi0u0@gmail.com>
2019-12-29 22:37:32 +0800

    update styles

--- components/blog/public/Banner.vue
+++ components/blog/public/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <v-sheet tile color="grey lighten-3">
+    <v-sheet tile color="grey lighten-4">
       <section class="app-banner ma-auto grey--text text--darken-1">
         <h3
           class="font-weight-medium banner-title px-5 px-sm-10 px-md-10 px-lg-0"
