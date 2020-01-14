c8b57a1660b18149cf2a4bf623000519c522432b
kyun <yezi0u0@gmail.com>
2020-01-14 23:33:59 +0800

    Category optimizing

--- components/blog/article-aside/Category.vue
+++ components/blog/article-aside/Category.vue
@@ -2,18 +2,24 @@
   <div>
     <h3>Categories</h3>
     <v-skeleton-loader
-      :loading="!this.$store.state.article.categories"
+      :loading="!categories"
       type="list-item-three-line,list-item-three-line,list-item-three-line"
     >
       <v-divider class="my-2" />
       <div class="p-2">
-        <category-item
-          v-for="category in this.$store.state.article.categories"
+        <div
+          v-for="category in categories"
           :key="category.id"
-          :item="category"
-          :depth="0"
+          :class="`category-item pb-1 pt-1 pl-${category.layer * 4}`"
         >
-        </category-item>
+          <nuxt-link
+            class="category-name pb-1 ml-1"
+            tag="span"
+            :to="{ name: 'search-category-id', params: { id: category.id } }"
+            >{{ category.name }}
+          </nuxt-link>
+          <span>({{ category.amount }})</span>
+        </div>
       </div>
       <v-divider class="my-2" />
     </v-skeleton-loader>
@@ -21,9 +27,49 @@
 </template>
 
 <script>
-import CategoryItem from '@/components/blog/article-aside/CategoryItem'
 export default {
   name: 'Category',
-  components: { CategoryItem }
+  data() {
+    return {
+      categories: []
+    }
+  },
+  mounted() {
+    const storeCategories = this.$store.state.article.categories || []
+    this.pushData(storeCategories, 0)
+  },
+  methods: {
+    pushData(storeCategories, layer) {
+      for (const category of storeCategories) {
+        this.categories.push({
+          id: category.id,
+          name: category.name,
+          layer,
+          amount: category.amount
+        })
+        if (category.subCategories && category.subCategories.length > 0) {
+          this.pushData(category.subCategories, layer + 1)
+        }
+      }
+    }
+  }
 }
 </script>
+
+<style scoped lang="scss">
+@import '~vuetify/src/styles/styles';
+
+.category-item {
+  line-height: 1.4rem;
+
+  .category-name {
+    cursor: pointer;
+    transition: border-bottom 0.25s;
+    border-bottom: 2px solid map-get($grey, lighten-5);
+
+    &:hover {
+      border-color: map-get($grey, base);
+    }
+  }
+}
+</style>
