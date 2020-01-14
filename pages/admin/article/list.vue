eedac1fb6d1dc45127d6c834c949a663985ac003
kyun <yezi0u0@gmail.com>
2020-01-14 22:56:15 +0800

    admin article page

--- pages/admin/article/list.vue
+++ pages/admin/article/list.vue
@@ -1,34 +1,106 @@
 <template>
   <div>
-    <v-data-table
-      :headers="headers"
-      :items="articles"
-      :options.sync="options"
-      :server-items-length="total"
-      :loading="loading"
-      class="elevation-1 mx-8"
-    ></v-data-table>
+    <v-card class="elevation-1 mx-8 my-8">
+      <v-card-title>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          :append-icon="mdiMagnify"
+          label="Search"
+          single-line
+          hide-details
+          style="max-width: 300px;"
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="articles"
+        :options.sync="options"
+        :server-items-length="total"
+        :loading="loading"
+      >
+        <template v-slot:header.views>
+          <v-icon small title="views" v-text="mdiChartBar" />
+        </template>
+        <template v-slot:header.commentAmount>
+          <v-icon small title="comments" v-text="mdiMessageReply" />
+        </template>
+
+        <template v-slot:item.tags="{ item }">
+          <v-chip
+            v-for="(tag, index) in item.tags"
+            :key="index"
+            class="mr-2"
+            x-small
+          >
+            {{ tag.name }}
+          </v-chip>
+        </template>
+        <template v-slot:item.postDate="{ item }">
+          <span :title="$moment(item.postDate).format('YYYY-MM-DD HH:mm:ss')">
+            {{ $moment(item.postDate).format('YYYY-MM-DD') }}
+          </span>
+        </template>
+        <template v-slot:item.action="{ item }">
+          <v-icon
+            small
+            class="mr-2"
+            title="edit"
+            @click="edit(item.id)"
+            v-text="mdiPencil"
+          />
+          <v-icon
+            small
+            title="delete"
+            @click="deleteItem(item.id)"
+            v-text="mdiDelete"
+          />
+        </template> </v-data-table
+    ></v-card>
   </div>
 </template>
 
 <script>
+import {
+  mdiDelete,
+  mdiPencil,
+  mdiChartBar,
+  mdiMessageReply,
+  mdiMagnify
+} from '@mdi/js'
+
 export default {
   name: 'List',
   layout: 'Admin',
   data() {
     return {
+      mdiPencil,
+      mdiDelete,
+      mdiChartBar,
+      mdiMessageReply,
+      mdiMagnify,
       total: 0,
       articles: [],
       loading: true,
       options: {},
       headers: [
-        { text: 'Title', value: 'name', align: 'left' },
-        { text: 'Desctiption', value: 'desctiption', sortable: false },
-        { text: 'Category', value: 'category', sortable: false },
-        { text: 'Tags', value: 'tags', sortable: false },
-        { text: 'Views', value: 'views' },
-        { text: 'Comments', value: 'comments' },
-        { text: 'Post Date', value: 'postDate' }
+        {
+          text: 'Actions',
+          value: 'action',
+          sortable: false,
+          width: 80
+        },
+        { text: 'Title', value: 'title' },
+        {
+          text: 'Category',
+          value: 'category.name',
+          sortable: false,
+          width: 110
+        },
+        { text: 'Tags', value: 'tags', sortable: false, width: '20%' },
+        { text: 'Views', value: 'views', width: 90 },
+        { text: 'Comments', value: 'commentAmount', width: 120 },
+        { text: 'Post Date', value: 'postDate', align: 'center', width: 120 }
       ]
     }
   },
@@ -58,6 +130,12 @@ export default {
     },
     getDesserts() {
       return []
+    },
+    edit(id) {
+      console.log(`edit:${id}`)
+    },
+    deleteItem(id) {
+      console.log(`delete:${id}`)
     }
   },
   head() {
